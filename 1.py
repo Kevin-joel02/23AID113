d36ba100d1b5101e2523b0b2dc3f8fc3b2a30aa6
KEVIN JOEL <ch.ai.u4aid25022@ch.students.amrita.edu>
2026-01-05 23:32:58 +0530

    DataTypes

---
+++ 1.py
@@ -0,0 +1,11 @@
+age = 20                  # integer
+height = 5.8              # float
+name = "Kevin"            # string
+are_you_student = True    # boolean
+
+print("----- Personal Details -----")
+print("Name           :", name)
+print("Age            :", age)
+print("Height         :", height)
+print("Are you student:", are_you_student)
+
